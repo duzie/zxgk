d6d6ad509fed86b19011856836d75430f32f17f3
fei <1623301>
2020-04-04 14:09:58 +0800

    first commit

---
+++ src/main/java/com/f/zxgk/FZxgkApplication.java
@@ -0,0 +1,13 @@
+package com.f.zxgk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FZxgkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FZxgkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/f/zxgk/FZxgkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f.zxgk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FZxgkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
