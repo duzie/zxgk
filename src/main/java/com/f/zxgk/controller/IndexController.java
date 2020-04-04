98ea3ba973657620e53d5d16fb6faa178b40f5b5
fei <1623301>
2020-04-04 23:49:02 +0800

    index page

---
+++ src/main/java/com/f/zxgk/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.f.zxgk.controller;
+
+import com.f.zxgk.service.ZxgkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @Autowired
+    private ZxgkService zxgkService;
+
+    @GetMapping
+    public String index() {
+        return "已收录数：" + zxgkService.count();
+    }
+}
