6e841651b6566a0b96de59fec8f2b1799bc391ac
fei <1623301>
2020-04-05 16:00:18 +0800

    页面查询

--- src/main/java/com/f/zxgk/controller/IndexController.java
+++ src/main/java/com/f/zxgk/controller/IndexController.java
@@ -1,19 +1,32 @@
 package com.f.zxgk.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.f.zxgk.po.Zxgk;
 import com.f.zxgk.service.ZxgkService;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class IndexController {
 
+
     @Autowired
     private ZxgkService zxgkService;
 
-    @GetMapping
+    @PostMapping("search")
+    @ResponseBody
+    public List<Zxgk> search(Zxgk zxgk) {
+        return zxgkService.lambdaQuery().like(Zxgk::getName, zxgk.getName())
+                .page(new Page<Zxgk>(1, 3)).getRecords();
+    }
+
+    @GetMapping("check")
     public String index() {
         Zxgk one = zxgkService.lambdaQuery().orderByDesc(Zxgk::getId).last("limit 1").one();
         String s = "已收录数：" + zxgkService.count();
